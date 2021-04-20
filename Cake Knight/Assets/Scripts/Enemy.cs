1675632e07fff4853be0e066f6dad35aadf88c54
Josh Plateros <48239318+joshplateros@users.noreply.github.com>
2021-04-19 18:48:28 -0700

    Added Knight basic attack, enemy scripts

---
+++ Cake Knight/Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    public int maxHealth = 100;
+    int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+    public void TakeDamage(int damage) {
+        currentHealth -= damage;
+
+        animator.SetTrigger("Take Damage");
+
+        if (currentHealth <= 0) {
+            Die();
+        }
+    }
+
+    void Die() {
+        animator.SetBool("isDead", true);
+        animator.SetTrigger("Die");
+        GetComponent<Collider>().enabled = false;
+
+        // Set script to off once enemy is dead
+        this.enabled = false;
+
+
+    }
+
+}
