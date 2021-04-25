ba7e79dfee306dba7552329c77b8bdde6704a114
Josh Plateros <48239318+joshplateros@users.noreply.github.com>
2021-04-24 22:40:39 -0700

    Updated attach script, added enemy behavior, squished edges

--- Cake Knight/Assets/PlatformAttach.cs
+++ Cake Knight/Assets/PlatformAttach.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PlatformAttach : MonoBehaviour
 {
     public GameObject Player;
+    public Rigidbody p;
+
+    private void Update() {
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -12,6 +16,14 @@ public class PlatformAttach : MonoBehaviour
         {
             Player.transform.parent = transform;
         }
+    }
+
+    private void OnTriggerStay(Collider other) {
+            if (p.velocity == Vector3.zero) {
+                Player.transform.parent = transform;
+            } else {
+                Player.transform.parent = null;
+            }
     }
 
     private void OnTriggerExit(Collider other)
