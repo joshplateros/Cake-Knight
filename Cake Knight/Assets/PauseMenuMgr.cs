80c2030308dac487c43ab752cabb15e4f0b0dbee
Brady Blundell <bradyzemailbox@gmail.com>
2021-04-27 22:04:27 -0700

    Pause Menu
    Added Pause menu with: Resume, Restart, and Exit.

---
+++ Cake Knight/Assets/PauseMenuMgr.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuMgr : MonoBehaviour
+{
+    public static bool isGamePaused = false;
+
+    public GameObject pauseMenuUi;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isGamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUi.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUi.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Resume();
+    }
+
+    public void ExitGame()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Resume();
+    }
+}
